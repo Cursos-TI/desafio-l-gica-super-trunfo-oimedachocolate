e3c18cf6adcbc7a11a9d21d04d2b64a71ff415f3
oimedachocolate <159135609+oimedachocolate@users.noreply.github.com>
2025-05-07 19:12:24 +0000

    F densidade e mostrar carta

--- logicaSuperTrunfoAventureiro.c
+++ logicaSuperTrunfoAventureiro.c
@@ -1,1 +1,33 @@
+#include  <stdio.h>
 
+//Estrutura da Carta
+typedef struct {
+    char codigo[10];
+    char nome[50];
+    char estado[30];
+    int populacao;
+    float  pib;
+    int pontos_turisticos;
+
+} Carta;
+
+// Função para calcular  a densidade populacional
+float calcularDensidade(Carta c) {
+    if (c.area   > 0) {
+        return c.populacao / c.area;
+    }
+    return 0;
+}
+
+// Função para mostrar uma carta
+void mostrarCarta(carta c)  {
+    printf("\nCódigo %s", c.codigo);
+    printf("Cidade: %s", c.nome);
+    printf("Estado: %s", c.estado);
+    printf("População: %d\n" c.populacao);
+    printf("Área: %.2f km²\n", c.area);
+    printf("PIB: %.2f bilhões\n", c.pib);
+    printf("Pontos Turísticos: %d\n", c.pontos_turisticos);
+    printf("Densidade Populacional: %.2f  hab/km²\n", calcularDensidade(c));
+    
+}
